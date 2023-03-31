f49350e30d1ec2a52092fc532d953943c97b4034
Vinicius Kalach <viniciuscato@hotmail.com>
2023-03-30 23:56:28 -0300

    desenvolvimento do ex 02 da list 02

--- AtividadesPOO2023/src/list02/exercise02/Policy.java
+++ AtividadesPOO2023/src/list02/exercise02/Policy.java
@@ -1,13 +1,86 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package list02.exercise02;
 
 /**
  *
- * @author Bruno
+ * @author Vinícius Kalach, RA: 211559
  */
 public class Policy {
     
+    private int number;
+    private String name;
+    private int age;
+    private char sex;
+    private double carValue;
+    
+    public Policy(){
+        
+    }
+    
+    public Policy(int number, String name, int age, char sex, double carValue) {
+        this.number = number;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.carValue = carValue;
+    }
+    
+    public double calcularValor() {
+        if (sex == 'M' && age <= 25) {
+            return carValue * 10/100;
+        } else if (sex == 'M' && age > 25) {
+            return carValue * 5/100;
+        } else if (sex == 'F') {
+            return carValue * 2/100;
+        }
+        return 0;
+    }
+    
+    public String imprimir() {
+        return "\nNúmero: " + number + 
+                "\nNome: " + name +
+                "\nIdade: " + age + 
+                "\nSaxo: " + sex + 
+                "\nValor do Automóvel: " + carValue + 
+                "\nValor do Apolice: " + calcularValor();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public double getCarValue() {
+        return carValue;
+    }
+
+    public void setCarValue(double carValue) {
+        this.carValue = carValue;
+    }
 }
--- AtividadesPOO2023/src/list02/exercise02/PolicyMain.java
+++ AtividadesPOO2023/src/list02/exercise02/PolicyMain.java
@@ -1,20 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package list02.exercise02;
+
+import java.util.Scanner;
 
 /**
  *
- * @author Bruno
+ * @author Vinícius Kalach, RA: 211559
  */
 public class PolicyMain {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        int i = 0;
+        Scanner read = new Scanner(System.in);
+        Policy p = new Policy();
+        
+        while (i != 3){
+            System.out.println("");
+            System.out.println("---Menu---");
+            System.out.println("Digite: ");
+            System.out.println("1 - criar apólice.");
+            System.out.println("2 - visualizar dados do apólice.");
+            System.out.println("3 - sair.");
+            System.out.println("----------");
+            System.out.println("");
+            i = Integer.parseInt(read.nextLine());
+            
+            switch (i) {
+                case 1:
+                    System.out.println("Digite o número.");
+                    p.setNumber(Integer.parseInt(read.nextLine()));
+                    System.out.println("Digite o nome.");
+                    p.setName(read.nextLine());
+                    System.out.println("Digite a idade.");
+                    p.setAge(Integer.parseInt(read.nextLine()));
+                    System.out.println("Digite o sexo.");
+                    p.setSex(read.nextLine().charAt(0));
+                    System.out.println("Digite o valor do automóvel.");
+                    p.setCarValue(read.nextFloat());
+                    break;
+                case 2:
+                    System.out.println(p.imprimir());
+                    break;
+                case 3:
+                    System.out.println("Saindo.");
+                    break;
+                default:
+                    System.out.println("Erro: opção inválida.");
+            }
+        }
+        
+        read.close();
     }
     
 }
