f29c435c39af24d3cbe3ede749e44339935306b0
Vinicius Kalach <viniciuscato@hotmail.com>
2023-04-20 23:59:22 -0300

    Complete classes (mobile and people)

--- AtividadesPOO2023/src/list03/exercise02/Automobile.java
+++ AtividadesPOO2023/src/list03/exercise02/Automobile.java
@@ -5,5 +5,37 @@ package list03.exercise02;
  * @author Vinícius Kalach, RA: 211559
  */
 public class Automobile {
+    private String brand;
+    private String model;
+    
+    public Automobile(){
+        
+    }
+    
+    public Automobile(String brand, String model){
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    
+    public String imprimir(){
+        return "\nMarca: " + brand   + 
+                "\nModel: " + model;
+    }
     
 }
--- AtividadesPOO2023/src/list03/exercise02/People.java
+++ AtividadesPOO2023/src/list03/exercise02/People.java
@@ -1,9 +1,69 @@
 package list03.exercise02;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Vinícius Kalach, RA: 211559
  */
 public class People {
+    private int code;
+    private String name;
+    private ArrayList<Automobile> automobile = 
+            new ArrayList<>();
+    
+    public People(){
+        
+    }
+    
+    public People(int code, String name){
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public ArrayList<Automobile> getAutomobile() {
+        return automobile;
+    }
+
+    public void setAutomobile(ArrayList<Automobile> automobile) {
+        this.automobile = automobile;
+    }
+    
+    public void inserirAutomóvel(Automobile automobile){
+        this.automobile.add(automobile);
+    }
+    
+    public void removerAutomóvel(int index){
+        this.automobile.remove(index);
+    }
+    
+    public String imprimir(){
+        return "\nCódigo: " + code   + 
+                "\nNome: " + name;
+    }
+    
+    public String imprimirCompleto(){
+        String retorno = imprimir();
+        for (Automobile automobile : automobile) {
+            retorno+= automobile.imprimir();
+        }
+        return retorno;
+    }
     
 }
